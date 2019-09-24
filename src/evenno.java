872fa5297b391cbbe83fc1a7e782c7915e2d16e0
sgkashid <sgkashid@gmail.com>
2019-09-24 19:51:23 +0530

    Even no program added

---
+++ src/evenno.java
@@ -0,0 +1,17 @@
+
+
+public class evenno {
+
+	public static void main(String[] args) {
+
+		int a = 1111;
+
+		if (a % 2 == 0) {
+			System.out.println("Given no is Even no");
+		} 
+		else {
+			System.out.println("Given no is Odd no");
+		}
+	}
+
+}
