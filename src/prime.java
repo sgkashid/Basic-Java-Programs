f8fff429caef136a33b2cb680443ae8e8bddc9b4
sgkashid <sgkashid@gmail.com>
2019-09-24 19:56:05 +0530

    Prime no identification program added

---
+++ src/prime.java
@@ -0,0 +1,22 @@
+
+
+public class prime {
+
+	public static void main(String[] args) {
+		int a=91, b, f=0;
+		for (b=2; b<a; b++)
+		{
+			if(a%b==0)
+			{
+				f=1;
+			}
+		}   
+		if(f==1)
+		{System.out.println( a+ ""
+				+ " is not prime");		}
+		else
+		{System.out.println( a+ " is prime");		}	
+		
+	}
+	
+}
