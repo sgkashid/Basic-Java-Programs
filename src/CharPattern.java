0481db9a6f1db26e2a238fb27d4d3e6739ea8add
sgkashid <sgkashid@gmail.com>
2019-09-24 19:42:37 +0530

    Char pattern pgm added

---
+++ src/CharPattern.java
@@ -0,0 +1,17 @@
+
+public class CharPattern {
+	
+	public static void main(String[] args) {
+		
+		String s="ABACABACACAB ABACAFADAGHAB";
+		int count=0;
+		for (int i = 0; i < s.length()-1; i++) {
+			if('A'==s.charAt(i) && 'B'==s.charAt(i+1)){
+				count++;
+			}
+		}
+		
+		System.out.println("Number of AB pattern repeated in String " + count);
+	}
+
+}
