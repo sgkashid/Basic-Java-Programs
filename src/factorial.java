d878fd170c163c4c5ce8a332a1d85a3a51c6be34
sgkashid <sgkashid@gmail.com>
2019-09-24 20:04:33 +0530

    Factorial pgm

---
+++ src/factorial.java
@@ -0,0 +1,22 @@
+
+
+public class factorial {
+	
+	public static void main(String[] args) {
+		
+		int i=6;
+		int c=1;
+	//for (i=6; i>0; i--)	{
+		for( int a=i; a>=1; a--){
+			c=a*c;	
+		}
+		System.out.println("Factorial of " + i + " is " +c);
+	c=1;
+	//}
+	}
+	
+	
+	
+	
+
+}
